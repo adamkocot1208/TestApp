9be6cb6d2fed4ae2be232b94013b9c56253759b8
adamkocot1208 <72563552+adamkocot1208@users.noreply.github.com>
2023-12-13 14:34:02 +0100

    Stworzenie aplikacji z dodatkowymi zadaniami

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿var numbers = new List<int>();
+int number;
+
+Console.WriteLine("Podaj 3 dowolne liczby całkowite: ");
+
+do
+{
+    Console.WriteLine("Podaj liczbę: ");
+    var numberString = Console.ReadLine();
+    var success = int.TryParse(numberString, out number );
+    if ( success )
+    {
+        numbers.Add( number );
+    }
+} while (numbers.Count < 3);
+
+Console.WriteLine("Właśnie wprowadziłeś 3 liczby.");
+
+var maxNumber = numbers.Max();
+
+Console.WriteLine(" ");
+Console.WriteLine("Największa liczba spośród podanych to: "+maxNumber.ToString());
